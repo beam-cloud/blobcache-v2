9d311bfd4fa80f1b83926e198a983bcead1ce627
Daniel Levi-Minzi <dleviminzi@gmail.com>
2025-02-28 12:48:04 -0500

    more debug logs

--- pkg/client.go
+++ pkg/client.go
@@ -352,8 +352,10 @@ func (c *BlobCacheClient) manageLocalClientCache(ttl time.Duration, interval tim
 }
 
 func (c *BlobCacheClient) IsCachedNearby(ctx context.Context, hash string) bool {
+	Logger.Infof("checking if hash is cached nearby: %s", hash)
 	hostAddrs, err := c.metadata.GetEntryLocations(ctx, hash)
 	if err != nil {
+		Logger.Errorf("error getting entry locations: %v", err)
 		return false
 	}
 
