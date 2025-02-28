da85556b098e33b1ad214b018501b589c2bb2d4f
Daniel Levi-Minzi <dleviminzi@gmail.com>
2025-02-28 15:35:59 -0500

    add logs

--- pkg/blobfs_node.go
+++ pkg/blobfs_node.go
@@ -134,6 +134,8 @@ func (n *FSNode) Lookup(ctx context.Context, name string, out *fuse.EntryOut) (*
 		return node, fs.OK
 	}
 
+	Logger.Infof("Lookup called with path: %s", fullPath)
+	Logger.Infof("FsID: %s", GenerateFsID(fullPath))
 	node, attr, err := n.inodeFromFsId(ctx, GenerateFsID(fullPath))
 	if err != nil {
 		return nil, syscall.ENOENT
